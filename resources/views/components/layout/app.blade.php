fe98608ef87b3038c4edca991f7b204662673d5f
“Frank <frank@frankjones.co>
2024-08-10 14:42:46 +0100

    Ran NPM build

--- resources/views/components/layout/app.blade.php
+++ resources/views/components/layout/app.blade.php
@@ -11,8 +11,11 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 
-    @vite('resources/css/app.css')
-    
+    {{-- @vite('resources/css/app.css') --}}
+
+    <link href="{{ asset('build/assets/app-CX6wZprs.css')}}"  rel="preload" as="style" onload="this.rel='stylesheet'">
+    <script src="{{ asset('build/assets/app-I5i9CKeh.js') }}" defer></script>
+
 </head>
 
 <body>
